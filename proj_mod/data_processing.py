e619ab240fbe57a5a0176e25065197e274435c7a
ycoeusz@arch <galasolarsxy@gmail.com>
2025-06-12 16:19:09 -0400

    created function that creates time series data of RV's for subintervals.

--- proj_mod/data_processing.py
+++ proj_mod/data_processing.py
@@ -4,6 +4,7 @@ import numpy as np
 # import sklearn
 
 def book_for_stock(str_file_path,stock_id,time_id,create_para=True):
+    #Created Yuan
     """
     A function that returns a pandas dataframe containing the book data of a stock specified in str_file_path with certain time_id. 
     The function defaulted to create the wap and log return of wap, but this can be turned off by setting create_para to False. 
@@ -29,6 +30,7 @@ def book_for_stock(str_file_path,stock_id,time_id,create_para=True):
     return df_raw_book
 
 def trade_for_stock(str_file_path,stock_id,time_id):
+    #Created Yuan
     """
     A function that returns a pandas dataframe containing the trade data of a stock at a chosen time_id. 
     
@@ -46,6 +48,8 @@ def trade_for_stock(str_file_path,stock_id,time_id):
     return df_raw_trade 
 
 def realized_vol(df_in,return_row_id=True): 
+    #Created Yuan
+    #Updated 06/12/25 Yuan
     """
     A function that returns the realized volatility based on the log return series input (for instance book["log_return"] where is book is a pandas dataframe containing book data for a stock and time id). 
     
@@ -56,15 +60,17 @@ def realized_vol(df_in,return_row_id=True):
     
     series_log_return=df_in["log_return"]
     rv=np.sqrt(np.sum(series_log_return**2))
-    stock_id=str(df_in["stock_id"][0])
-    time_id=str(df_in["time_id"][0])
-    row_id=stock_id+"-"+time_id
-    if return_row_id:
+    if (return_row_id & (len(df_in)==0)):
+        print("Can not harvest srock_id and time_id")
+    if (return_row_id & (len(df_in)!=0)):
+        stock_id=str(df_in["stock_id"].iloc[0])
+        time_id=str(df_in["time_id"].iloc[0])
+        row_id=stock_id+"-"+time_id
         return rv, row_id
-    else: 
-        return rv
+    return rv
     
 def time_cross_val_split(list_time,n_split=4,percent_val_size=10): 
+    #Created Yuan
     """
     A function that take in a list of time id and return a time series split for cross validation, this function is written due to issue with sklearn.model_selection.TimeSeriesSplit. 
     
@@ -98,3 +104,32 @@ def time_cross_val_split(list_time,n_split=4,percent_val_size=10):
     # return enumerate(tscv.split(list_time))
 
     return enumerate(return_list)
+
+def create_RV_timeseries(df_in, n_subint=60, in_start=0, in_end=600) -> np.array: 
+    #Created 06/12/25 Yuan
+    """
+    A function that created RV for subintervals of the whole interval for a chosen stock_id and time_id. 
+    It is expected that (in_end-in_start)%n_subint==0. 
+    :param df_in: A pandas dataframe with "log_return" and "seconds_in_bucket" columns. 
+    :param n_subint: Integer number of total intervals wanted, defaulted to 60. 
+    :param in_start: Integer start of the start of time interval, defaulted to 0. 
+    :param in_end: Integer end of the end of time interval, defaulted to 600. 
+    :return: A numpy array of length n_subint populated by RV of each sub-interval. 
+    """
+    
+    in_len=in_end-in_start
+    # print("length of interval is",in_len)
+    if (in_len%n_subint): 
+        # print("Both the length of interval and the total number of sub-interval are expected to be integers. The length of interval is not divisible by number of subinterval, returning None.")
+        return None
+    int_sublen=int(in_len/n_subint)
+    arr_RV=np.zeros(n_subint)
+    # print("length each sub-interval is", int_sublen)
+    for index in range(n_subint):
+        subin_start=index*int_sublen
+        subin_end=(index+1)*int_sublen
+        subin_RV=realized_vol(df_in=df_in[(df_in["seconds_in_bucket"]>subin_start)&(df_in["seconds_in_bucket"]<=subin_end)],return_row_id=False)
+        # print("RV of the sub-interval",(index+1),"is",subin_RV)
+        arr_RV[index]=subin_RV
+        # print(arr_RV)
+    return arr_RV
