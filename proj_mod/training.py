83b8cef9ba279210629b2fbda1559f17f643a533
ycoeusz@arch <galasolarsxy@gmail.com>
2025-06-17 13:08:37 -0400

    Created training.py, more EDA done.

---
+++ proj_mod/training.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+def rmspe(y_true, y_pred):
+    #Created 06/17/25 Yuan
+    '''
+    Compute Root Mean Square Percentage Error between two arrays.
+    '''
+    loss = np.sqrt(np.mean(np.square(((y_true - y_pred) / y_true)), axis=0))
+
+    return loss
+
+def rmspe_of_linear(x,y): 
+    #Created 06/17/25 Yuan
+    """
+    Find rmspe when predicting y with x by linear regression. 
+    """
+    model=LinearRegression()
+    model.fit(x,y)
+    pred_linear=model.predict(x)
+    return rmspe(y_pred=pred_linear,y_true=y)
+
+def rmspe_linear_df(df,list_feature,str_target):
+    #Created 06/17/25 Yuan
+    """
+    Find rmspe when predicting target column of dataframe with features in dataframe by linear regression. 
+    """
+    return rmspe_of_linear(df[list_feature],df[[str_target]])
