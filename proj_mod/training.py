4c956814b4a0adbe2aa4de007599d1458b9d81c5
ycoeusz@arch <galasolarsxy@gmail.com>
2025-06-25 15:55:41 -0400

    created rmspe loss function for pytorch

--- proj_mod/training.py
+++ proj_mod/training.py
@@ -1,5 +1,9 @@
 import numpy as np
+import torch
+import torch.nn as nn
+import sys
 from sklearn.linear_model import LinearRegression
+
 
 def rmspe(y_true, y_pred):
     '''
@@ -23,3 +27,16 @@ def rmspe_linear_df(df,list_feature,str_target):
     Find rmspe when predicting target column of dataframe with features in dataframe by linear regression. 
     """
     return rmspe_of_linear(df[list_feature],df[[str_target]])
+
+class RMSPELoss(nn.Module): 
+    #Created 06/25/25 
+    """
+    RMSPE is not a premade pytorch loss function, the following creates the loss function so that it can be used in training. 
+    """
+    def __init__(self,eps=sys.float_info.epsilon): 
+        super(RMSPELoss,self).__init__()
+        self.eps=eps
+        
+    def forward(self, ypred, ytrue): 
+        return torch.sqrt(torch.mean(torch.square((ypred-ytrue)/(ytrue+self.eps))))
+        
