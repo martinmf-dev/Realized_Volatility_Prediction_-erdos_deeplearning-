667efedeba8b14ede42b06959ff86f452a8bf65d
ycoeusz@arch <galasolarsxy@gmail.com>
2025-08-07 16:12:38 -0700

    updated ETLpipeline

--- ETLpipeline/Default_Extract_Transform.py
+++ ETLpipeline/Default_Extract_Transform.py
@@ -12,11 +12,11 @@ from proj_mod import data_processing, recover_time_id
 
 def Default_Ext_Trans():
     #Getting raw data
-    get_raw=input("Would you like to download raw data from kaggle?(Y/N)").upper()
+    get_raw=input("Would you like to download raw data from kaggle (expected to be done in 240 seconds)?(Y/N)\n").upper()
     if get_raw=="Y": 
         print("Retrieving raw data from kaggle: \n")
         kaggle.api.authenticate()
-        print("Starting to download, this will take a while ...")
+        print("Starting to download ...")
         start_time = time.time()
         os.system("kaggle competitions download -c optiver-realized-volatility-prediction -p ../raw_data")
         print("Downloaded data to \"../raw_data folder\"")
@@ -28,11 +28,12 @@ def Default_Ext_Trans():
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(f"Raw data received from kaggle in {elapsed_time} seconds. Stored in \"./raw_data/kaggle_ORVP\". Contents of this folder will be ignored by git.\n")
+    print("-"*50) 
     
     #Recoverying time
-    recover_time=input("Would you like to recover time id order?(Y/N)").upper()
+    recover_time=input("Would you like to recover time id order (expected to be done in 60 seconds)?(Y/N)\n").upper()
     if recover_time=="Y": 
-        print("Recovering time id ordering, this will take a while ...\n")
+        print("Recovering time id ordering ...\n")
         start_time = time.time()
         recovered_time_id_order_list = recover_time_id.reconstruct_time_id_order(str_path="../raw_data/kaggle_ORVP/book_train.parquet")
         recovered_time_id_order = np.array(recovered_time_id_order_list, dtype=int)
@@ -41,72 +42,92 @@ def Default_Ext_Trans():
         print(f"Time id order recovered in {elapsed_time} seconds. Starting to save it. \n")
         np.save('../processed_data/recovered_time_id_order.npy',recovered_time_id_order)
         print("Recovered time id order saved to \"../processed_data/recovered_time_id_order.npy\". Contents of this folder will be ignored by git.\n")
+    print("-"*50) 
         
     #Creating data with default set ups. 
-    create_data=input("Would you like to transform raw data into ready-to-use data?(Y/N)").upper()
+    create_data=input("Would you like to transform raw data into ready-to-use data?(Y/N)\n").upper()
     if create_data=="Y": 
         print("Transforming data...\n")
         
-        print("Creating RV by row_id, expected to be done in 60 seconds ...\n")
-        path_book="../raw_data/kaggle_ORVP/book_train.parquet"
-        start_time = time.time()
-        df_rv=data_processing.create_df_RV_by_row_id_parallel(path_book)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Created RV by row id data if {elapsed_time} seconds.\n")
-        df_rv.to_parquet("../processed_data/RV_by_row_id.parquet",index=False)
-        print("Saved to \"../processed_data/RV_by_row_id.parquet\". Contents of this folder will be ignored by git.\n")
-        save_RV_csv=input("Would you like to save it also as csv?(Y/N)").upper()
-        if save_RV_csv=="Y": 
-            df_rv.to_csv("../processed_data/RV_by_row_id.csv",index=False)
-            print("Saved to \"../processed_data/RV_by_row_id.csv\". Contents of this folder will be ignored by git.\n")
+        create_RV=input("Create RV by row id (expected to be done in 60 seconds)?(Y/N)\n").upper()
         
-        print("Creating trade values by row id, expected to be done in 10 seconds ...\n")
-        start_time = time.time()
-        df_tab=data_processing.create_df_trade_vals_by_row_id(str_path="../raw_data/kaggle_ORVP/trade_train.parquet")
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Created trade values by row id data in {elapsed_time} seconds.")
-        df_tab.to_parquet("../processed_data/trade_vals_by_row_id.parquet",index=False)
-        print("Saved to \"../processed_data/trade_vals_by_row_id.parquet\". Contents of this folder will be ignored by git.\n")
-        save_trade_csv=input("Would you like to save it also as csv?(Y/N)").upper()
-        if save_trade_csv=="Y": 
-            df_tab.to_csv("../processed_data/trade_vals_by_row_id.csv",index=False)
+        if create_RV=="Y":
+            print("Creating RV by row_id ...\n")
+            path_book="../raw_data/kaggle_ORVP/book_train.parquet"
+            start_time = time.time()
+            df_rv=data_processing.create_df_RV_by_row_id_parallel(path_book)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Created RV by row id data if {elapsed_time} seconds.\n")
+            df_rv.to_parquet("../processed_data/RV_by_row_id.parquet",index=False)
+            print("Saved to \"../processed_data/RV_by_row_id.parquet\". Contents of this folder will be ignored by git.\n")
+            save_RV_csv=input("Would you like to save it also as csv?(Y/N)").upper()
+            if save_RV_csv=="Y": 
+                df_rv.to_csv("../processed_data/RV_by_row_id.csv",index=False)
+                print("Saved to \"../processed_data/RV_by_row_id.csv\". Contents of this folder will be ignored by git.\n")
+        print("-"*50) 
         
-        print("Creating trade values grouped by time id and row id separately respectively, expected to be done in 10 seconds ...")
-        start_time = time.time()
-        df_total=data_processing.create_df_param_emb_by_group(df_in=df_tab,str_groupby="time_id",dict_params_aggfun={"price_mean":["mean","std"],"size_sum":["mean","std"],"order_count_sum":["mean","std"]})
-        df_total=data_processing.create_df_param_emb_by_group(df_in=df_total,str_groupby="stock_id",dict_params_aggfun={"price_mean":["mean","std"],"size_sum":["mean","std"],"order_count_sum":["mean","std"]})
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Created trade values grouped by time and stock in {elapsed_time} seconds.")
-        df_total.to_parquet("../processed_data/total_trade_values.parquet",index=False)
-        print("Saved to \"../processed_data/total_trade_values.parquet\". Contents of this folder will be ignored by git.\n")
-        save_total_csv=input("Would you like to save it also as csv, this file will be large (hence, advised against)?(Y/N)").upper()
-        if save_total_csv=="Y":
-            df_total.to_csv("../processed_data/total_trade_values.csv",index=False)
-            
-        print("Creating timeseries data, expected to be done in 360 seconds ...")
-        start_time = time.time()
-        path_book="../raw_data/kaggle_ORVP/book_train.parquet"
-        book_time=data_processing.create_timeseries(path_book,dict_agg={"log_return":data_processing.rv},dict_rename={"log_return":"sub_int_RV"})
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Created timeseries data in {elapsed_time:.2f} seconds.")
-        book_time.to_parquet("../processed_data/book_RV_ts_60_si.parquet")
-        print("Saved to \"../processed_data/book_RV_ts_60_si.parquet\". Contents of this folder will be ignored by git.\n")
-        save_time_csv=input("Would you like to save it also as csv, this file will be VERY large (hence, HIGHLY advised against)?(Y/N)").upper()
-        if save_time_csv=="Y": 
-            book_time.to_csv("../processed_data/book_RV_ts_60_si.csv")
+        create_trade=input("Create trade data (expected to be done in 20 seconds)?(Y/N)\n").upper()
+        
+        if create_trade=="Y":
+            print("Creating trade values by row id ...\n")
+            start_time = time.time()
+            df_tab=data_processing.create_df_trade_vals_by_row_id(str_path="../raw_data/kaggle_ORVP/trade_train.parquet")
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Created trade values by row id data in {elapsed_time} seconds.")
+            df_tab.to_parquet("../processed_data/trade_vals_by_row_id.parquet",index=False)
+            print("Saved to \"../processed_data/trade_vals_by_row_id.parquet\". Contents of this folder will be ignored by git.\n")
+            save_trade_csv=input("Would you like to save it also as csv?(Y/N)").upper()
+            if save_trade_csv=="Y": 
+                df_tab.to_csv("../processed_data/trade_vals_by_row_id.csv",index=False)
+            print("-"*50) 
+        
+            print("Creating trade values grouped by time id and row id separately respectively ...")
+            start_time = time.time()
+            df_total=data_processing.create_df_param_emb_by_group(df_in=df_tab,str_groupby="time_id",dict_params_aggfun={"price_mean":["mean","std"],"size_sum":["mean","std"],"order_count_sum":["mean","std"]})
+            df_total=data_processing.create_df_param_emb_by_group(df_in=df_total,str_groupby="stock_id",dict_params_aggfun={"price_mean":["mean","std"],"size_sum":["mean","std"],"order_count_sum":["mean","std"]})
+            #Create embedding id
+            unique_ids = sorted(df_total['stock_id'].unique())
+            id_to_emb = {stock_id: i for i, stock_id in enumerate(unique_ids)}
+            df_total['emb_id'] = df_total['stock_id'].map(id_to_emb)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Created trade values grouped by time and stock in {elapsed_time} seconds.")
+            df_total.to_parquet("../processed_data/total_trade_values.parquet",index=False)
+            print("Saved to \"../processed_data/total_trade_values.parquet\". Contents of this folder will be ignored by git.\n")
+            save_total_csv=input("Would you like to save it also as csv, this file will be large (hence, advised against)?(Y/N)").upper()
+            if save_total_csv=="Y":
+                df_total.to_csv("../processed_data/total_trade_values.csv",index=False)
+        print("-"*50) 
+                
+        create_ts=input("Create timesereis data (expected to be done in 360 seconds)?(Y/N)\n").upper()
+        
+        if create_ts=="Y":
+            print("Creating timeseries data ...")
+            start_time = time.time()
+            path_book="../raw_data/kaggle_ORVP/book_train.parquet"
+            book_time=data_processing.create_timeseries(path_book,dict_agg={"log_return":data_processing.rv},dict_rename={"log_return":"sub_int_RV"})
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Created timeseries data in {elapsed_time:.2f} seconds.")
+            book_time.to_parquet("../processed_data/book_RV_ts_60_si.parquet")
+            print("Saved to \"../processed_data/book_RV_ts_60_si.parquet\". Contents of this folder will be ignored by git.\n")
+            save_time_csv=input("Would you like to save it also as csv, this file will be VERY large (hence, HIGHLY advised against)?(Y/N)").upper()
+            if save_time_csv=="Y": 
+                book_time.to_csv("../processed_data/book_RV_ts_60_si.csv")
+        print("-"*50) 
         
     return 0
     
 print("In this process, raw data will be downloaded from kaggle. Afterwhich, default relevant data will be extracted and transformed with python pandas code with SQL like queries which will then be safed on file.\n")
 start=input("Start the process?(Y/N)\n").upper()
+print("-"*50) 
 if start=="Y": 
     print("If this is the first time you run this code, please input Y to all options (the options, when advised against, to save csv file is optional, even if this is the fitst time you run the code). Operation starting ...\n")
     Default_Ext_Trans()
     print("Process finished. Please keep in mind that, due to the structure of pytorch, the loading procedure of the ETL pipeline will be done through training.RVdataset to create pytorch Dataset subclass so that it can be loaded into training loop with pytorch dataloader.")
-else: 
-    print("Exiting the process")
+    print("-"*50) 
+print("Exiting the process")
+print("-"*50) 
 
