f2174394c63364c360e4dfacf255e49f8253a5a0
ycoeusz@arch <galasolarsxy@gmail.com>
2025-08-07 12:57:50 -0700

    debugged the Extract and Transform pipeline.

--- ETLpipeline/Default_Extract_Transform.py
+++ ETLpipeline/Default_Extract_Transform.py
@@ -1,8 +1,11 @@
-import subprocess
+# import subprocess
 import sys
 import numpy as np
 import time
 import pandas as pd
+import kaggle 
+import os
+import zipfile 
 
 sys.path.append("../")
 from proj_mod import data_processing, recover_time_id
@@ -12,16 +15,27 @@ def Default_Ext_Trans():
     get_raw=input("Would you like to download raw data from kaggle?(Y/N)").upper()
     if get_raw=="Y": 
         print("Retrieving raw data from kaggle: \n")
-        subprocess.call(["python data_collecting_kaggle.py"],cwd="./data_collecting/", shell=True)
+        kaggle.api.authenticate()
+        print("Starting to download, this will take a while ...")
+        os.system("kaggle competitions download -c optiver-realized-volatility-prediction -p ../raw_data")
+        print("Downloaded data to \"../raw_data folder\"")
+        with zipfile.ZipFile("../raw_data/optiver-realized-volatility-prediction.zip","r") as zip_ref:
+            zip_ref.extractall("../raw_data/kaggle_ORVP")
+        print("Unzipped to ../raw_data/kaggle_ORVP")
+        os.remove("../raw_data/optiver-realized-volatility-prediction.zip")
+        print("Removed file ../raw_data/optiver-realized-volatility-prediction.zip")
         print("Raw data received from kaggle. Stored in \"./raw_data/kaggle_ORVP\". Contents of this folder will be ignored by git.\n")
     
     #Recoverying time
     recover_time=input("Would you like to recover time id order?(Y/N)").upper()
     if recover_time=="Y": 
-        print("Recovering time id ordering...\n")
+        print("Recovering time id ordering, this will take a while ...\n")
+        start_time = time.time()
         recovered_time_id_order_list = recover_time_id.reconstruct_time_id_order(str_path="../raw_data/kaggle_ORVP/book_train.parquet")
         recovered_time_id_order = np.array(recovered_time_id_order_list, dtype=int)
-        print("Time id order recovered. Starting to save it. \n")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Time id order recovered in {elapsed_time} seconds. Starting to save it. \n")
         np.save('../processed_data/recovered_time_id_order.npy',recovered_time_id_order)
         print("Recovered time id order saved to \"../processed_data/recovered_time_id_order.npy\". Contents of this folder will be ignored by git.\n")
         
@@ -30,7 +44,7 @@ def Default_Ext_Trans():
     if create_data=="Y": 
         print("Transforming data...\n")
         
-        print("Creating RV by row_id ...\n")
+        print("Creating RV by row_id, expected to be done in 60 seconds ...\n")
         path_book="../raw_data/kaggle_ORVP/book_train.parquet"
         start_time = time.time()
         df_rv=data_processing.create_df_RV_by_row_id_parallel(path_book)
@@ -44,7 +58,7 @@ def Default_Ext_Trans():
             df_rv.to_csv("../processed_data/RV_by_row_id.csv",index=False)
             print("Saved to \"../processed_data/RV_by_row_id.csv\". Contents of this folder will be ignored by git.\n")
         
-        print("Creating trade values by row id ...\n")
+        print("Creating trade values by row id, expected to be done in 10 seconds ...\n")
         start_time = time.time()
         df_tab=data_processing.create_df_trade_vals_by_row_id(str_path="../raw_data/kaggle_ORVP/trade_train.parquet")
         end_time = time.time()
@@ -56,7 +70,7 @@ def Default_Ext_Trans():
         if save_trade_csv=="Y": 
             df_tab.to_csv("../processed_data/trade_vals_by_row_id.csv",index=False)
         
-        print("Creating trade values grouped by time id and row id separately respectively ...")
+        print("Creating trade values grouped by time id and row id separately respectively, expected to be done in 10 seconds ...")
         start_time = time.time()
         df_total=data_processing.create_df_param_emb_by_group(df_in=df_tab,str_groupby="time_id",dict_params_aggfun={"price_mean":["mean","std"],"size_sum":["mean","std"],"order_count_sum":["mean","std"]})
         df_total=data_processing.create_df_param_emb_by_group(df_in=df_total,str_groupby="stock_id",dict_params_aggfun={"price_mean":["mean","std"],"size_sum":["mean","std"],"order_count_sum":["mean","std"]})
@@ -69,7 +83,7 @@ def Default_Ext_Trans():
         if save_total_csv=="Y":
             df_total.to_csv("../processed_data/total_trade_values.csv",index=False)
             
-        print("Creating timeseries data...")
+        print("Creating timeseries data, expected to be done in 360 seconds ...")
         start_time = time.time()
         path_book="../raw_data/kaggle_ORVP/book_train.parquet"
         book_time=data_processing.create_timeseries(path_book,dict_agg={"log_return":data_processing.rv},dict_rename={"log_return":"sub_int_RV"})
