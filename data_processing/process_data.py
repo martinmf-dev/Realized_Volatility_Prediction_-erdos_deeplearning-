c2c6d652ebd2dfbe69ae53c784e35ff4d5f6b6c1
ycoeusz@arch <galasolarsxy@gmail.com>
2025-08-07 17:00:47 -0700

    deleted a file

--- data_processing/process_data.py
+++
@@ -1,5 +0,0 @@
-#A file to create all processed data needed for training, kept empty for now during EDA. 
-
-#To include: creat current RV for each time bucket. 
-
-#To include: creat trade data for each time bucket. 
